e44901f471cb4e2cd48319329886a4b2efc38cb1
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-01-10 14:50:28 +0100

    add app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const express = require('express')
-const app = express()
+// # Config
+const express = require("express");
+const app = express();
+const domain = `localhost`;
+const port = 3000;
 
+// # Middlewares
+app.use(express.static("public"));
+app.use(express.json());
+
+// # Routes
+const moviesRouter = require(`./routers/movies`);
+
+app.use(`/movies`, moviesRouter);
+
+// # Listening
+app.listen(port, () => {
+  console.log(`Server listening on port ${domain}:${port}`);
+});
