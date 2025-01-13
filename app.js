9c1ca918fb1965b68b044edd523a25f1141b3bef
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-01-13 16:44:17 +0100

    add cors options

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const cors = require("cors");
 const app = express();
 
 require("dotenv").config();
+const domain = process.env.APP_HOST;
+const port = process.env.APP_PORT;
+const appUrl = process.env.APP_FRONTEND_URL;
 
-const domain = process.env.HOST_DOMAIN;
-const port = process.env.HOST_PORT;
+const corsOptions = {
+  origin: appUrl,
+  optionsSuccessStatus: 200,
+};
 
 // # Middlewares
 const errorsHandler = require("./middlewares/errorsHandler");
 const notFound = require("./middlewares/notFound");
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // # Routes
 const pagesRouter = require(`./routers/pages`);
